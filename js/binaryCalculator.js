e68f2793938e4ec817d7ed317c840fdb47c24c3a
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2022-08-07 20:16:46 +0100

    building calculating logic

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,5 +1,5 @@
 let operand1 = [];
-let operand2 = [" "];
+let operand2 = [""];
 let operater;
 let operaterClicked = false;
 
@@ -9,7 +9,7 @@ btn0.addEventListener("click", () => {
     res.innerHTML = operand1.join("");
   } else if (operaterClicked) {
     operand2.push(0);
-    res.innerHTML = `${operand1.join("")}${operater}${operand2.join("")}`;
+    res.innerHTML = `${operand1.join("")}${operater}${operand2?.join("")}`;
   }
 });
 
@@ -19,37 +19,38 @@ btn1.addEventListener("click", () => {
     res.innerHTML = operand1.join("");
   } else if (operaterClicked) {
     operand2.push(1);
-    res.innerHTML = `${operand1.join("")}${operater}${operand2.join("")}`;
+    res.innerHTML = `${operand1.join("")}${operater}${operand2?.join("")}`;
   }
 });
 
 btnSum.addEventListener("click", () => {
   operaterClicked = true;
   operater = "+";
-  res.innerHTML = `${operand1.join("")}${operater}${operand2.join("")}`;
+  res.innerHTML = `${operand1.join("")}${operater}${operand2?.join("")}`;
 });
 
 btnSub.addEventListener("click", () => {
   operaterClicked = true;
   operater = "-";
-  res.innerHTML = `${operand1.join("")}${operater}${operand2.join("")}`;
+  res.innerHTML = `${operand1.join("")}${operater}${operand2?.join("")}`;
 });
 
 btnMul.addEventListener("click", () => {
   operaterClicked = true;
   operater = "*";
-  res.innerHTML = `${operand1.join("")}${operater}${operand2.join("")}`;
+  res.innerHTML = `${operand1.join("")}${operater}${operand2?.join("")}`;
 });
 
 btnDiv.addEventListener("click", () => {
   operaterClicked = true;
   operater = "/";
-  res.innerHTML = `${operand1.join("")}${operater}${operand2.join("")}`;
+  res.innerHTML = `${operand1.join("")}${operater}${operand2?.join("")}`;
 });
 
 btnClr.addEventListener("click", () => {
   operand1 = [];
   operand2 = [];
-  operater = null;
+  operaterClicked = false;
+  operater;
   res.innerHTML = "";
 });
